79b193045f3f89abacfac2c9589478280f62451f
조강현 <jokh980324@naver.com>
2026-05-20 16:53:41 +0900

    결정론적 물리 주사위 추가
    Closes #6

--- Assets/_Project/Script/DicePhysics/CubeDice.cs
+++ Assets/_Project/Script/DicePhysics/CubeDice.cs
@@ -1,41 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VectorGraphics;
 using UnityEngine;
 
-public class CubeDice : MonoBehaviour
+public class CubeDice : DiceObject
 {
-    public List<Quaternion> faceQuaternion;
-    public List<TextMeshPro> diceText;
-
-    Rigidbody rb;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    private void Start()
-    {
-        StartCoroutine(Roll(2));
-    }
-
-    public void TextSet(List<string> data)
-    {
-        for (int i = 0; i < 6; i++)
-        {
-            diceText[i].text = data[i];
-        }
-    }
-
-    public IEnumerator Roll(int face)
-    {
-        rb.linearVelocity = Random.insideUnitSphere * 3f;
-        rb.angularVelocity = Random.insideUnitSphere * 10f;
-
-        yield return new WaitUntil(() => rb.IsSleeping());
-
-        rb.isKinematic = true;
-        transform.rotation = faceQuaternion[face];
-    }
 }
+
